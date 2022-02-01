319c2d711cf50dc21614ad6f29163ca030e5ac2c
leesh5000 <leesh50000@gmail.com>
2022-02-02 00:45:37 +0900

    add loadbyusername

--- backend/src/main/java/leesh/devcom/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/leesh/devcom/backend/security/CustomUserDetailsService.java
@@ -1,11 +1,18 @@
 package leesh.devcom.backend.security;
 
+import leesh.devcom.backend.domain.Member;
 import leesh.devcom.backend.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 @RequiredArgsConstructor
 @Service
@@ -14,10 +21,17 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException());
 
+        List<GrantedAuthority> authorities = singletonList(new SimpleGrantedAuthority("ROLE_USER"));
 
-        return null;
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .authorities(authorities)
+                .build();
     }
 }
