ed082cca6276aec3c5d2919a2f9f01cf2546f2ae
leesh5000 <leesh50000@gmail.com>
2022-02-02 00:14:32 +0900

    add initdb

---
+++ backend/src/main/java/leesh/devcom/backend/InitDb.java
@@ -0,0 +1,36 @@
+package leesh.devcom.backend;
+
+import leesh.devcom.backend.domain.Member;
+import leesh.devcom.backend.domain.MemberRepository;
+import leesh.devcom.backend.security.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static leesh.devcom.backend.domain.Member.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class InitDb {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    CommandLineRunner loadInitData() {
+        return args -> {
+            log.info("init db start...");
+            memberRepository.save(createMember("test1@gmail.com", "test1", passwordEncoder.encode("1111")));
+            memberRepository.save(createMember("test2@gmail.com", "test2", passwordEncoder.encode("1111")));
+            memberRepository.save(createMember("test3@gmail.com", "test3", passwordEncoder.encode("1111")));
+            memberRepository.save(createMember("test4@gmail.com", "test4", passwordEncoder.encode("1111")));
+            memberRepository.save(createMember("test5@gmail.com", "test5", passwordEncoder.encode("1111")));
+            memberRepository.save(createMember("test6@gmail.com", "test6", passwordEncoder.encode("1111")));
+            log.info("init db done...");
+        };
+    }
+}
