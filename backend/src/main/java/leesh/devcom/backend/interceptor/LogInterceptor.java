a2d878b91f21e6507d33ae53ad33e31d2f61f728
leesh5000 <leesh50000@gmail.com>
2022-02-13 04:12:55 +0900

    add LogInterceptor

--- backend/src/main/java/leesh/devcom/backend/interceptor/LogInterceptor.java
+++ backend/src/main/java/leesh/devcom/backend/interceptor/LogInterceptor.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 import java.util.UUID;
 
 @Slf4j
@@ -35,6 +36,7 @@ public class LogInterceptor implements HandlerInterceptor {
         }
 
         log.info("request preHandle [logId = {}][requestUri = {}][Handler = {}]", logId, requestURI, hm);
+        log.info("getParameters = {}", getParameters(request));
         return true;
     }
 
@@ -43,7 +45,7 @@ public class LogInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, Exception e) throws Exception {
 
         String requestURI = request.getRequestURI();
         String logId = (String) request.getAttribute(LOG_ID);
@@ -52,8 +54,45 @@ public class LogInterceptor implements HandlerInterceptor {
         Long elapsedTime = System.currentTimeMillis() - startTime;
 
         log.info("request afterCompletion [logId = {}][requestUri = {}][Handler = {}][total elapsed milliseconds = {}]", logId, requestURI, handler, elapsedTime);
-        if (ex != null) {
-            log.error("afterCompletion error [logId = {}]", logId, ex);
+        if (e != null) {
+            log.error("afterCompletion error [logId = {}]", logId, e);
         }
+    }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuffer posted = new StringBuffer();
+        Enumeration<?> e = request.getParameterNames();
+        if (e != null) {
+            posted.append("?");
+        }
+        while (e.hasMoreElements()) {
+            if (posted.length() > 1) {
+                posted.append("&");
+            }
+            String curr = (String) e.nextElement();
+            posted.append(curr + "=");
+            if (curr.contains("password")
+                    || curr.contains("pass")
+                    || curr.contains("pwd")) {
+                posted.append("*****");
+            } else {
+                posted.append(request.getParameter(curr));
+            }
+        }
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+        if (ipAddr!=null && !ipAddr.equals("")) {
+            posted.append("&_psip=" + ipAddr);
+        }
+        return posted.toString();
+    }
+
+    private String getRemoteAddr(HttpServletRequest request) {
+        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+        if (ipFromHeader != null && ipFromHeader.length() > 0) {
+            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+            return ipFromHeader;
+        }
+        return request.getRemoteAddr();
     }
 }
