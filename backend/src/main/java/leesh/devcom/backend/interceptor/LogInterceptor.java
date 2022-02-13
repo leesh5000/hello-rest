f5930c9881f21dda300b9ef449d32b926a007fd1
leesh5000 <leesh50000@gmail.com>
2022-02-13 23:10:29 +0900

    code polishing

--- backend/src/main/java/leesh/devcom/backend/interceptor/LogInterceptor.java
+++ backend/src/main/java/leesh/devcom/backend/interceptor/LogInterceptor.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
 import java.util.UUID;
 
 @Slf4j
@@ -22,7 +21,7 @@ public class LogInterceptor implements HandlerInterceptor {
     public static final String START_TIME = "startTime";
 
     @Override
-    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
 
         String requestURI = request.getRequestURI();
         String logId = UUID.randomUUID().toString();
@@ -35,17 +34,16 @@ public class LogInterceptor implements HandlerInterceptor {
             hm = (HandlerMethod) handler;
         }
 
-        log.info("request preHandle [logId = {}][requestUri = {}][Handler = {}]", logId, requestURI, hm);
-        log.info("getParameters = {}", getParameters(request));
+        log.info("request preHandle [logId = {}][requestUri = {}][Handler = {}][remoteIp = {}]", logId, requestURI, hm, request.getRemoteAddr());
         return true;
     }
 
     @Override
-    public void postHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, ModelAndView modelAndView) {
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, Exception e) throws Exception {
+    public void afterCompletion(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, Exception e) {
 
         String requestURI = request.getRequestURI();
         String logId = (String) request.getAttribute(LOG_ID);
@@ -57,42 +55,5 @@ public class LogInterceptor implements HandlerInterceptor {
         if (e != null) {
             log.error("afterCompletion error [logId = {}]", logId, e);
         }
-    }
-
-    private String getParameters(HttpServletRequest request) {
-        StringBuffer posted = new StringBuffer();
-        Enumeration<?> e = request.getParameterNames();
-        if (e != null) {
-            posted.append("?");
-        }
-        while (e.hasMoreElements()) {
-            if (posted.length() > 1) {
-                posted.append("&");
-            }
-            String curr = (String) e.nextElement();
-            posted.append(curr + "=");
-            if (curr.contains("password")
-                    || curr.contains("pass")
-                    || curr.contains("pwd")) {
-                posted.append("*****");
-            } else {
-                posted.append(request.getParameter(curr));
-            }
-        }
-        String ip = request.getHeader("X-FORWARDED-FOR");
-        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
-        if (ipAddr!=null && !ipAddr.equals("")) {
-            posted.append("&_psip=" + ipAddr);
-        }
-        return posted.toString();
-    }
-
-    private String getRemoteAddr(HttpServletRequest request) {
-        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
-        if (ipFromHeader != null && ipFromHeader.length() > 0) {
-            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
-            return ipFromHeader;
-        }
-        return request.getRemoteAddr();
     }
 }
