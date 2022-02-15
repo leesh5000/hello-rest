28f17491de7e9d3eb50aba71fdab4d2953b3401b
leesh5000 <leesh50000@gmail.com>
2022-02-15 23:47:49 +0900

    JwtUtilTest test success

--- backend/src/test/java/leesh/devcom/backend/security/JwtUtilTest.java
+++ backend/src/test/java/leesh/devcom/backend/security/JwtUtilTest.java
@@ -1,23 +1,14 @@
 package leesh.devcom.backend.security;
 
-import leesh.devcom.backend.domain.Member;
-import leesh.devcom.backend.domain.MemberRepository;
 import leesh.devcom.backend.dto.RegisterRequest;
 import leesh.devcom.backend.service.MemberService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest
 class JwtUtilTest {
@@ -36,6 +27,11 @@ class JwtUtilTest {
         memberService.save(RegisterRequest.builder().email("test1@gmail.com").username("test1").password("1111").build());
         memberService.save(RegisterRequest.builder().email("test2@gmail.com").username("test2").password("1111").build());
         memberService.save(RegisterRequest.builder().email("test3@gmail.com").username("test3").password("1111").build());
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberService.deleteAll();
     }
 
     @Test
